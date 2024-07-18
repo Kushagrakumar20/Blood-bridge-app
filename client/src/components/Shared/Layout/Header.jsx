150ef3bd80f59488d5509b042b5d413d4a592b9d
unknown <kushagrakumar0210@gmail.com>
2024-07-18 11:55:19 +0530

    nav bar

---
+++ client/src/components/Shared/Layout/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { MdBloodtype } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom';
+
+const Header = () => {
+    const {user} = useSelector(state => state.auth)
+
+
+    // Logout handler
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.clear()
+        alert("Logout successfully")
+        navigate('/login')
+    }
+  return (
+    <>
+      <nav className='navbar'>
+        <div className="container-fluid">
+            <div className="navbar-brand"><MdBloodtype color='red'/>  Blood Bridge App</div>
+            <ul className="navbar-nav flex-row">
+                <li className="nav-item mx-3">
+                    <p className="nav-link">
+                    <FaUsers /> Welcome {user?.name} 
+                    </p> 
+                </li>
+                <li className="nav-item mx-3">
+                    <button className="btn btn-danger" onClick={handleLogout}>
+                        Logout
+                    </button>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
